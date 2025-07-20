e733b98e5772eda10bcba3106a56a6f25aafb04f
Avazkhan <avazkhanhasanov0@gmail.com>
2025-07-21 03:26:09 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/day_25_vazifa/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.day_25_vazifa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(Day25App());
+}
+
+class Day25App extends StatelessWidget {
+  const Day25App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
